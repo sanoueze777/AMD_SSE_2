1b65ebd42a806fe18e7f5f21bf4e0722659d425f
sanoueze777 <79368786+sanoueze777@users.noreply.github.com>
2023-05-31 14:44:41 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -108,21 +108,6 @@ def update_expert_planifie(conn, task):
     conn.commit()
 
     return cur.lastrowid
-
-def select_all_actions(conn):
-    """
-    Create a new project into the projects table
-    :param conn:
-    :param project:
-    :return: project id
-    """
-    sql = '''SELECT * from Action_executee'''
-    cur = conn.cursor(buffered=True)
-    cur.execute(sql)
-    conn.commit()
-    return cur
-
-
 def update_mission_executee(conn, task):
     """
     Create a new task
@@ -180,13 +165,6 @@ def select_from_experts(conn):
     cur.execute(sql)
     conn.commit()
     return cur
-def highlight_survived(s):
-    return ['background-color: lightgreen']*len(s) if 50<s else ['background-color: pink']*len(s)
-
-def color_survived(val):
-    color = 'green' if 50<val else 'pink'
-    return f'background-color: {color}'
-
 """"""""""""
 
 
@@ -231,10 +209,7 @@ if authentication_status:
     conn = create_connection(database)
     records1 = select_from_mission(conn).fetchall()
     records2 = select_from_experts(conn).fetchall()
-
-    experts_table = select_all_actions(conn).fetchall()
-    missions_table = select_all_actions(conn).fetchall()
-
+    
     missions_list = []
     experts_list = []
     
@@ -262,7 +237,7 @@ if authentication_status:
     st.sidebar.image("Capture d’écran 2022-10-17 175305.png", use_column_width=False)
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
     with st.sidebar:
-            choice = option_menu(None,["Accueil", 'Mes Activités'], 
+            choice = option_menu(None,["Accueil", 'Suivi du personnel',"Suivi des missions"], 
         icons=['house','list-task','gear'], default_index=0,styles={
             "nav-link-selected": {"background-color": "red"},
         })
@@ -270,9 +245,51 @@ if authentication_status:
     st.sidebar.title(f"{expert_name} ")
     st.sidebar.markdown('###')
     st.sidebar.markdown("### Parametres")
-    
-    
-    
+    start_year, end_year = st.sidebar.slider(
+        "Période",
+        min_value=min_year, max_value=max_year,
+        value=(min_year, max_year))
+
+    st.sidebar.markdown('###')
+    origins = st.sidebar.multiselect('Origins', origin_list,
+                                     default=origin_list)
+    st.sidebar.markdown('###')
+    item1 = st.sidebar.selectbox('element 1', item_list, index=0)
+    item2 = st.sidebar.selectbox('element 2', item_list, index=3)
+
+    df_rng = df[(df['YYYY'] >= start_year) & (df['YYYY'] <= end_year)]
+    source = df_rng[df_rng['Origin'].isin(origins)]
+
+    # Content
+    base = alt.Chart(source).properties(height=300)
+
+    bar = base.mark_bar().encode(
+        x=alt.X('count(Origin):Q', title='Number of Records'),
+        y=alt.Y('Origin:N', title='Origin'),
+        color=alt.Color('Origin:N', legend=None)
+    )
+
+    point = base.mark_circle(size=50).encode(
+        x=alt.X(item1 + ':Q', title=item1),
+        y=alt.Y(item2 + ':Q', title=item2),
+        color=alt.Color('Origin:N', title='',
+                        legend=alt.Legend(orient='bottom-left'))
+    )
+
+    line1 = base.mark_line(size=5).encode(
+        x=alt.X('yearmonth(Year):T', title='Date'),
+        y=alt.Y('mean(' + item1 + '):Q', title='exemple'),
+        color=alt.Color('Origin:N', title='',
+                        legend=alt.Legend(orient='bottom-left'))
+    )
+
+    line2 = base.mark_line(size=5).encode(
+        x=alt.X('yearmonth(Year):T', title='Date'),
+        y=alt.Y('mean(' + item2 + '):Q', title='exemple'),
+        color=alt.Color('Origin:N', title='',
+                        legend=alt.Legend(orient='bottom-left'))
+    )
+
     # Layout (Content)
     Main_df = pd.DataFrame({"Mission": [""], "CHAMP D'ACTIVITES": [""], "TEMPS DE TRAVAIL (Part de la journée)": [None], "LIEU": [""]})
 
@@ -327,34 +344,26 @@ if authentication_status:
 
 
 
-    if choice == "Mes Activités":
-        experts_table = pd.DataFrame(experts_table, columns = ["Numero","Expert","Date","Mission","Activité","Temps de travail","Lieu de travail" ])
-        
-        experts_table["taux d'exécution %"] = 0
-        for i in range(len(experts_table)):
-            if experts_table.loc[i,"Activité"] == "Réunions cadrage /démarrage":
-                experts_table.loc[i,"taux d'exécution %"] = 5
-            elif experts_table.loc[i,"Activité"] == "Atelier cadrage":
-                experts_table.loc[i,"taux d'exécution %"] = 5
-            elif experts_table.loc[i,"Activité"] == "Outils collecte":
-                experts_table.loc[i,"taux d'exécution %"] = 15
-            elif experts_table.loc[i,"Activité"] == "Collecte données 1 (entretiens )":
-                experts_table.loc[i,"taux d'exécution %"] = 30
-            elif experts_table.loc[i,"Activité"] == "Collecte données 2 (Revue doc/BD)":
-                experts_table.loc[i,"taux d'exécution %"] = 45
-            elif experts_table.loc[i,"Activité"] == "Reunion étape mise en œuvre":
-                experts_table.loc[i,"taux d'exécution %"] = 60
-            elif experts_table.loc[i,"Activité"] == "Rapports d'étude":
-                experts_table.loc[i,"taux d'exécution %"] = 80
-            elif experts_table.loc[i,"Activité"] == "Atelier examen /validation":
-                experts_table.loc[i,"taux d'exécution %"] = 100
-            else:
-                experts_table.loc[i,"taux d'exécution %"] = None
-                
-                
-        table_summary = experts_table.groupby("Mission")["taux d'exécution %"].max()
-        table_summary = table_summary.to_frame()
-        st.dataframe(table_summary.style.applymap(color_survived, subset=["taux d'exécution %"]))
+    if choice == "Suivi du personnel":
+        left_column, right_column = st.columns(2)
+        df = pd.DataFrame(
+            np.random.randn(100, 2) / [1, 1] + [17.573934, -3.9861092],
+            columns=['lat', 'lon'])
+
+        left_column.map(df)
+        left_column.markdown(
+            '**Indicateur  1 exemple')
+        left_column.altair_chart(bar, use_container_width=True)
+
+        right_column.markdown(
+            '**Indicateur 2 exemple' )
+        right_column.altair_chart(point, use_container_width=True)
+
+        left_column.markdown( '_ (Indicateur  3 exemple )**')
+        left_column.altair_chart(line1, use_container_width=True)
+
+        right_column.markdown('_ (Indicateur  4 exemple)**')
+        right_column.altair_chart(line2, use_container_width=True)
 
 elif authentication_status is False:
     st.error('Username/password is incorrect')
